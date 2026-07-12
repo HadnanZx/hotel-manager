aae9a9a2ac75cbd6db559a2f711e1ddf0372c0f3
Hadnan Menezes <hadnanmn.00@gmail.com>
2026-07-12 12:44:13 -0300

    Criação do Metodo de Criar reservas

--- trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/business/GerenciadorReservas.java
+++ trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/business/GerenciadorReservas.java
@@ -6,20 +6,34 @@ import br.edu.ifba.saj.ads.poo.model.Quarto;
 import br.edu.ifba.saj.ads.poo.model.Reserva;
 import br.edu.ifba.saj.ads.poo.model.Quarto;
 import br.edu.ifba.saj.ads.poo.model.Data;
+import br.edu.ifba.saj.ads.poo.model.Hospede;
 
 public class GerenciadorReservas {
     private ArrayList<Reserva> reservas = new ArrayList<Reserva>();
 
     public boolean temConflito(Quarto quartoNovo, Data checkinNovo, Data checkoutNovo) {
-    for (Reserva reservaExistente : reservas) {
-        if (reservaExistente.getQuarto() == quartoNovo) {
-            boolean semConflito = checkoutNovo.anterior(reservaExistente.getCheckin()) 
+        for (Reserva reservaExistente : reservas) {
+            if (reservaExistente.getQuarto() == quartoNovo) {
+                boolean semConflito = checkoutNovo.anterior(reservaExistente.getCheckin()) 
                                 || checkinNovo.posterior(reservaExistente.getCheckout());
-            if (!semConflito) {
-                return true;
+                if (!semConflito) {
+                    return true;
+                }
             }
         }
+        return false;
     }
-    return false;
-}
+
+
+    public boolean criarReserva(Hospede hospede, Quarto quarto, Data checkin, Data checkout) {
+        if (!checkout.posterior(checkin)) {
+            return false;
+        }
+        if (temConflito(quarto, checkin, checkout)) {
+            return false;
+        }
+        Reserva novaReserva = new Reserva(hospede, quarto, checkin, checkout);
+        reservas.add(novaReserva);
+        return true;
+    }
 }
