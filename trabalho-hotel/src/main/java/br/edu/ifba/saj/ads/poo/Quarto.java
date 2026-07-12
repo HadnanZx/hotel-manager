619f2f6a6d7d32b09a1bf5c7817a81c6de6282b6
Hadnan Menezes <hadnanmn.00@gmail.com>
2026-07-12 10:42:32 -0300

    Criação da Classe Quarto e seu Enum

---
+++ trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/Quarto.java
@@ -0,0 +1,25 @@
+package br.edu.ifba.saj.ads.poo;
+
+public class Quarto {
+    private int numero;
+    private TipoQuarto tipo;
+    private double preco;
+
+    public Quarto(int numero, TipoQuarto tipo, double preco){
+        this.numero = numero;
+        this.tipo = tipo;
+        this.preco = preco;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+     public TipoQuarto getTipo(){
+        return tipo;
+    }
+     public double getPreco(){
+        return preco;
+    }
+
+
+}
