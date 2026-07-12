0b47d9252c1d63c13bf049f80968e5e8ab5c2ffa
Hadnan Menezes <hadnanmn.00@gmail.com>
2026-07-12 13:26:16 -0300

    Organização de pastas e criação de resources para a parte visual do projeto

--- trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/App.java
+++ trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/App.java
@@ -1,13 +1,34 @@
 package br.edu.ifba.saj.ads.poo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label labelNome = new Label("Nome:");
+        TextField campoNome = new TextField();
+
+        Label labelCpf = new Label("CPF:");
+        TextField campoCpf = new TextField();
+
+        Button botaoCadastrar = new Button("Cadastrar");
+
+        VBox raiz = new VBox(labelNome, campoNome, labelCpf, campoCpf, botaoCadastrar);
+
+        Scene cena = new Scene(raiz, 400, 300);
+        stage.setScene(cena);
+        stage.setTitle("Cadastro de Hóspede");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
