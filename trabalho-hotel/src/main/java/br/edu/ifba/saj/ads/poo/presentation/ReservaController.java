b9bec35d8804d1c3928e3c860b8c0fd7971bd4c3
Hadnan Menezes <hadnanmn.00@gmail.com>
2026-08-09 23:24:43 -0300

    Re-criando ReservaController 2/2

--- trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/presentation/ReservaController.java
+++ trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/presentation/ReservaController.java
@@ -2,14 +2,14 @@ package br.edu.ifba.saj.ads.poo.presentation;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Alert;
+import br.edu.ifba.saj.ads.poo.business.GerenciadorReservas;
 import java.time.LocalDate;
+import br.edu.ifba.saj.ads.poo.model.Data;
 import br.edu.ifba.saj.ads.poo.model.Hospede;
 import br.edu.ifba.saj.ads.poo.model.Quarto;
-import br.edu.ifba.saj.ads.poo.model.Data;
-import br.edu.ifba.saj.ads.poo.business.GerenciadorReservas;
 
 public class ReservaController {
 
@@ -22,40 +22,22 @@ public class ReservaController {
     @FXML
     private DatePicker dataCheckin;
 
+    
     @FXML
     private DatePicker dataCheckout;
 
+    
     @FXML
     private Button botaoReservar;
 
     private GerenciadorReservas gerenciador;
 
-    public void setGerenciador(GerenciadorReservas gerenciador) {
+    public void setGerenciador(GerenciadorReservas gerenciador){
         this.gerenciador = gerenciador;
         comboHospede.getItems().addAll(gerenciador.getHospedes());
         comboQuarto.getItems().addAll(gerenciador.getQuartos());
     }
 
-    @FXML
-    private void reservar() {
-        Hospede hospedeEscolhido = comboHospede.getValue();
-        Quarto quartoEscolhido = comboQuarto.getValue();
-        LocalDate checkinEscolhido = dataCheckin.getValue();
-        LocalDate checkoutEscolhido = dataCheckout.getValue();
+    
 
-        Data checkin = new Data(checkinEscolhido.getDayOfMonth(), checkinEscolhido.getMonthValue(), checkinEscolhido.getYear());
-        Data checkout = new Data(checkoutEscolhido.getDayOfMonth(), checkoutEscolhido.getMonthValue(), checkoutEscolhido.getYear());
-
-        boolean sucesso = gerenciador.criarReserva(hospedeEscolhido, quartoEscolhido, checkin, checkout);
-
-        Alert alerta;
-        if (sucesso) {
-            alerta = new Alert(Alert.AlertType.INFORMATION);
-            alerta.setContentText("Reserva criada com sucesso!");
-        } else {
-            alerta = new Alert(Alert.AlertType.ERROR);
-            alerta.setContentText("Não foi possível criar a reserva. Verifique as datas e a disponibilidade do quarto.");
-        }
-        alerta.show();
-    }
 }
