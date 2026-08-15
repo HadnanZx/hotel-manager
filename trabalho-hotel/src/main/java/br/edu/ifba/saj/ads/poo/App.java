e0141b08fd28bcca20d05b8be235713546a9ffa6
Hadnan Menezes <hadnanmn.00@gmail.com>
2026-08-15 16:09:38 -0300

    O mostrarCadastro e mostrarReserva adicionados no MainController

--- trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/App.java
+++ trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import br.edu.ifba.saj.ads.poo.business.GerenciadorReservas;
 import br.edu.ifba.saj.ads.poo.presentation.CadastroHospedeController;
+import br.edu.ifba.saj.ads.poo.presentation.MainController;
 import br.edu.ifba.saj.ads.poo.presentation.ReservaController;
 
 public class App extends Application {
@@ -15,13 +16,22 @@ public class App extends Application {
     private Stage stagePrincipal;
 
     @Override
+    
     public void start(Stage stage) throws Exception {
         this.stagePrincipal = stage;
-        abrirTelaCadastro();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent raizPrincipal = loader.load();
+
+        MainController mainController = loader.getController();
+        mainController.setApp(this);
+        mainController.mostrarCadastro();
+
+        Scene cena = new Scene(raizPrincipal, 700, 400);
+        stage.setScene(cena);
         stage.setTitle("Sistema do Hotel");
         stage.show();
     }
-
     public void abrirTelaCadastro() throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("cadastro-hospede.fxml"));
         Parent raiz = loader.load();
@@ -41,6 +51,26 @@ public class App extends Application {
         controller.setGerenciador(gerenciador);
 
         stagePrincipal.setScene(new Scene(raiz, 400, 400));
+    }
+
+    public Parent carregarCadastro() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("cadastro-hospede.fxml"));
+        Parent raiz = loader.load();
+
+        CadastroHospedeController controller = loader.getController();
+        controller.setGerenciador(gerenciador);
+
+        return raiz;
+    }
+
+    public Parent carregarReserva() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("reserva-quarto.fxml"));
+        Parent raiz = loader.load();
+
+        ReservaController controller = loader.getController();
+        controller.setGerenciador(gerenciador);
+
+        return raiz;
     }
 
     public static void main(String[] args) {
--- trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/presentation/MainController.java
+++ trabalho-hotel/src/main/java/br/edu/ifba/saj/ads/poo/presentation/MainController.java
@@ -1,5 +1,6 @@
 package br.edu.ifba.saj.ads.poo.presentation;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 import br.edu.ifba.saj.ads.poo.App;
 
@@ -10,5 +11,20 @@ public class MainController {
     private BorderPane painelPrincipal;
 
     private App app;
+
+    public void setApp(App app){
+        this.app = app;
+    }
     
+    @FXML
+    private void mostrarCadastro() throws Exception{
+        Parent conteudo = app.carregarCadastro();
+        painelPrincipal.setCenter(conteudo);
+    }
+
+    @FXML
+    private void mostrarReserva() throws Exception{
+        Parent conteudo = app.carregarReserva();
+        painelPrincipal.setCenter(conteudo);
+    }
 }
